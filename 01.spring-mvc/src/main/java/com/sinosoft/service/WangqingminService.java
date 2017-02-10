e2152eca178957fdc166db563701b3aeaa6052ea
王清民 <wangqingmin@sinosoft.com.cn>
2017-02-10 19:16:56 +0800

    测试

---
+++ 01.spring-mvc/src/main/java/com/sinosoft/service/WangqingminService.java
@@ -0,0 +1,8 @@
+package com.sinosoft.service;
+
+/**
+ * Created by WQM on 2017/2/10.
+ */
+public interface WangqingminService {
+    public String wangqingmin(String z);
+}
---
+++ 01.spring-mvc/src/main/java/com/sinosoft/service/WangqingminServiceImpl.java
@@ -0,0 +1,13 @@
+package com.sinosoft.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by WQM on 2017/2/10.
+ */
+@Service//业务层注解
+public class WangqingminServiceImpl implements WangqingminService {
+    public String wangqingmin(String z) {
+        return z;
+    }
+}
