56544172acca07cfdd97afb1dc71b0194da579ec
李高明 <ligaoming11294@sinosoft.com.cn>
2017-02-11 12:27:02 +0800

    测试

--- 01.spring-mvc/src/main/java/com/sinosoft/controller/LigaomingController.java
+++ 01.spring-mvc/src/main/java/com/sinosoft/controller/LigaomingController.java
@@ -1,6 +1,9 @@
 package com.sinosoft.controller;
 
 import com.sinosoft.service.LigaomingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,11 +11,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 /**
  * Created by ligm on 2017/2/9.
  */
+@Controller
 public class LigaomingController {
+    @Autowired
     private LigaomingService ligaomingService;
-    @RequestMapping(path = "/ligm/{params}",method = RequestMethod.GET)
+    @RequestMapping(path = "/test/{a}",method = RequestMethod.GET)//设置访问地址
     @ResponseBody
-    public String Hello(String a){
-        return ligaomingService.test(a);
+    public String Hello(@PathVariable String a,String b){//@PathVariable 取路径中的变量
+        b = " world!";
+        return ligaomingService.test(a,b);
     }
 }
