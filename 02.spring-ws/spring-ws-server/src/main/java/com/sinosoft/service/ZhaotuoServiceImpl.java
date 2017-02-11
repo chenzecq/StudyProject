110349e0f585f1c4312307328235880816c2fc19
赵拓 <zhaotuo@sinosoft.com.cn>
2017-02-11 17:42:29 +0800

    zhaotuo

---
+++ 02.spring-ws/spring-ws-server/src/main/java/com/sinosoft/service/ZhaotuoServiceImpl.java
@@ -0,0 +1,13 @@
+package com.sinosoft.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Administrator on 2017/2/11.
+ */
+@Service
+public class ZhaotuoServiceImpl implements ZhatuoService{
+    public String dealMsg(String msg) {
+        return msg;
+    }
+}
