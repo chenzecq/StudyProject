40176349064bab099eec6869e847ad08d2753b68
尚雷 <shanglei@sinosoft.com.cn>
2017-02-11 10:23:56 +0800

    Spring-mvc 测试小程序

---
+++ 01.spring-mvc/src/main/java/com/sinosoft/service/ShangleiService.java
@@ -0,0 +1,8 @@
+package com.sinosoft.service;
+
+/**
+ * Created by Administrator on 2017/2/10.
+ */
+public interface ShangleiService {
+    public String shanglei(String s);
+}
---
+++ 01.spring-mvc/src/main/java/com/sinosoft/service/ShangleiServiceImpl.java
@@ -0,0 +1,13 @@
+package com.sinosoft.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Administrator on 2017/2/10.
+ */
+@Service
+public class ShangleiServiceImpl implements ShangleiService{
+    public String shanglei(String s ){
+        return s;
+    }
+}
