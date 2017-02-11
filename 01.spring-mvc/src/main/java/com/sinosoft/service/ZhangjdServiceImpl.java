cd1aa9c1dda5e14299fc6c50f27843015d46c2b5
张建东 <zhangjd13343@sinosoft.com.cn>
2017-02-11 23:24:53 +0800

    spring-mvc 测试

---
+++ 01.spring-mvc/src/main/java/com/sinosoft/service/ZhangjdServiceImpl.java
@@ -0,0 +1,14 @@
+package com.sinosoft.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Jarod on 2017/2/9.
+ */
+@Service
+public class ZhangjdServiceImpl implements ZhangjdService{
+
+    public String Zhangjd(String z ){
+        return z;
+    }
+}
