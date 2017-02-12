6a58d8d18e103f4c9be8bc0da17cdef0cedc01ce
wei <weixiangyou@sinosoft.com.cn>
2017-02-12 20:15:26 +0800

    魏向有/Webservice服务端

---
+++ 02.spring-ws/spring-ws-server/src/main/java/com/sinosoft/service/WeixiangyouServiceImpl.java
@@ -0,0 +1,11 @@
+package com.sinosoft.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by wei on 2017/2/12.
+ */
+@Service
+public class WeixiangyouServiceImpl implements WeixiangyouService {
+    public String dealMsg(String pMsg) {return pMsg;}
+}
