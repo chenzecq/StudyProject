4ce982b017e2000d2522756326fd20b835dd62f2
袁文博 <yuanwenbo@sinosoft.com.cn>
2017-02-12 22:06:40 +0800

    完成02.开发任务SpringMVC

---
+++ 01.spring-mvc/src/main/java/com/sinosoft/service/YuanwenboService.java
@@ -0,0 +1,10 @@
+package com.sinosoft.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by ywb02 on 2017/2/12.
+ */
+public interface YuanwenboService {
+    String yuanwenbo(String ywb);
+}
