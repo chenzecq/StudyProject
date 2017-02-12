71571bb8c4164cabfe42521c97aafd728b9d9df3
郭忠华 <guozhonghua@sinosoft.com.cn>
2017-02-12 20:47:26 +0800

    webservice测试

---
+++ 02.spring-ws/spring-ws-server/src/test/java/com/sinosoft/ws/GuozhonghuaWSImplTest.java
@@ -0,0 +1,61 @@
+package com.sinosoft.ws;
+
+import org.apache.cxf.endpoint.Client;
+import org.apache.cxf.jaxws.endpoint.dynamic.JaxWsDynamicClientFactory;
+import org.apache.cxf.testutil.common.AbstractBusTestServerBase;
+import org.junit.Before;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import javax.xml.ws.Endpoint;
+
+import static org.apache.cxf.testutil.common.AbstractClientServerTestBase.launchServer;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by guozhonghua on 2017/2/12.
+ */
+@ContextConfiguration(
+        locations = {
+                "classpath:spring/spring-context.xml"
+        })
+@WebAppConfiguration
+public class GuozhonghuaWSImplTest extends AbstractJUnit4SpringContextTests {
+
+    public static class SimpleServer extends AbstractBusTestServerBase{
+        protected void run() {
+            Object implementor = new GuozhonghuaWSImpl();
+            String address = "http://localhost:" + 8080 + "/soap/GuozhonghuaWS";
+            Endpoint.publish(address, implementor);
+        }
+        public static void main(String[] args) {
+            try {
+                SimpleServer s = new SimpleServer();
+                s.start();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                System.exit(-1);
+            } finally {
+                System.out.println("done!");
+            }
+        }
+    }
+
+    @org.junit.Test
+    public void sendMsg() throws Exception {
+        JaxWsDynamicClientFactory dynamicClient = JaxWsDynamicClientFactory.newInstance();
+        Client client = dynamicClient.createClient("http://localhost:8080/soap/GuozhonghuaWS?wsdl");
+        Object[] objects = client.invoke("sendMsg", "0204");
+        assertTrue(objects!=null);
+        assertTrue(objects.length==1);
+        assertTrue(objects[0]!=null);
+        assertTrue(objects[0].equals("0204"));
+
+    }
+
+    @Before
+    public void init() {
+        launchServer(SimpleServer.class, true);
+    }
+}
