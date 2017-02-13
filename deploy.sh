1853783dd5ce2ef191a4354e6be80f9caea7c8de
杨明 <yangming@sinosoft.com.cn>
2017-02-13 13:18:01 +0800

    添加演示。

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+cp ./01.spring-mvc/target/01.spring-mvc-1.0-SNAPSHOT.war spring-mvc.war
+cp ./02.spring-ws/spring-ws-server/target/spring-ws-server-1.0-SNAPSHOT.war spring-ws-server.war
