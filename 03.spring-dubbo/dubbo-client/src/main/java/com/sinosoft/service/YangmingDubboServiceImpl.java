5e558f793317f1cd48fb6180231c35ef9159f03c
杨明 <yangming@sinosoft.com.cn>
2017-02-14 16:58:43 +0800

    添加Dubbo的说明文档。

--- 03.spring-dubbo/dubbo-client/src/main/java/com/sinosoft/service/YangmingDubboServiceImpl.java
+++ 03.spring-dubbo/dubbo-client/src/main/java/com/sinosoft/service/YangmingDubboServiceImpl.java
@@ -3,11 +3,12 @@ package com.sinosoft.service;
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.sinosoft.api.YangmingApi;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by yangming on 2017/2/12.
  */
-@Component //要注意这是Spring的
+@Service //要注意这是Spring的
 public class YangmingDubboServiceImpl implements YangmingDubboService {
 
     @Reference
