46cd6230e71a3e555443f21f95b31ac13be0fb6a
heshouyou <heshouyou@sinosoft.com.cn>
2017-02-15 20:53:22 +0800

    webservice服务

---
+++ 02.spring-ws/spring-ws-server/src/main/java/com/sinosoft/service/HeshouyouServiceImpl.java
@@ -0,0 +1,15 @@
+package com.sinosoft.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Administrator on 2017/2/15/015.
+ */
+@Service
+public class HeshouyouServiceImpl implements HeshouyouService{
+
+    @Override
+    public String dealReqMsg(String msg) {
+        return msg;
+    }
+}
