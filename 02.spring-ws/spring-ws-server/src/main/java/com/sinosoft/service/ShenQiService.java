2e9cf5e39683dd87783a668ea687fb48fc10a062
沈琦 <shenqi@sinosoft.com.cn>
2017-02-15 22:13:17 +0800

    ws测试

---
+++ 02.spring-ws/spring-ws-server/src/main/java/com/sinosoft/service/ShenQiService.java
@@ -0,0 +1,10 @@
+package com.sinosoft.service;
+
+/**
+ * Created by shenqi on 2017/2/15.
+ */
+public interface ShenQiService {
+
+    public String dealData(String data);
+
+}
---
+++ 02.spring-ws/spring-ws-server/src/main/java/com/sinosoft/service/ShenQiServiceImpl.java
@@ -0,0 +1,14 @@
+package com.sinosoft.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by shenqi on 2017/2/15.
+ */
+@Service
+public class ShenQiServiceImpl implements ShenQiService{
+
+    public String dealData(String data) {
+        return data;
+    }
+}
