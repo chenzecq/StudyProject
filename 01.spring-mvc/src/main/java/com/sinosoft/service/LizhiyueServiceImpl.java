917d4ad869edf7c057785686b0eab67493139cdf
lizhiyue <lizhiyue@sinosoft.com.cn>
2017-02-15 19:11:34 +0800

    lizhiyue

---
+++ 01.spring-mvc/src/main/java/com/sinosoft/service/LizhiyueServiceImpl.java
@@ -0,0 +1,14 @@
+package com.sinosoft.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by lizhiyue on 2017-2-09
+ */
+@Service
+public class LizhiyueServiceImpl implements LizhiyueService{
+
+    public String lizhiyue(String param1) {
+        return param1;
+    }
+}
