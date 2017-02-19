ac818e63b2ead318a7d47206bbe4dcc46b32dd65
郭洪岳 <guohongyue12186@sinosoft.com.cn>
2017-02-19 23:07:49 +0800

    注解

--- 02.spring-ws/spring-ws-server/src/main/java/com/sinosoft/service/ZhangyafengServiceImpl.java
+++ 02.spring-ws/spring-ws-server/src/main/java/com/sinosoft/service/ZhangyafengServiceImpl.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Service;
 /**
  * Created by sinosoft.zyf on 2017/2/11.
  */
-
 @Service
 public class ZhangyafengServiceImpl implements ZhangyafengService {
     public String dealMsg(String msg) {
