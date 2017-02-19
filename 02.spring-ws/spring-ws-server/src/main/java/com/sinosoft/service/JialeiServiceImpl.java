45b032309852e3ecca17ec408fb9c688790a0980
贾磊 <jialei14345@sinosoft.com.cn>
2017-02-19 22:38:46 +0800

    ws 练习

---
+++ 02.spring-ws/spring-ws-server/src/main/java/com/sinosoft/service/JialeiServiceImpl.java
@@ -0,0 +1,13 @@
+package com.sinosoft.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Theron on 2017/2/19.
+ */
+@Service
+public class JialeiServiceImpl implements JialeiService{
+    public String dealMsg(String msg) {
+        return msg;
+    }
+}
